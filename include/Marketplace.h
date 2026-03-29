74fc0290b0b361cdc0c174fb125ab4c057c39f22
SerobCholakyan <260147726+SerobCholakyan@users.noreply.github.com>
2026-03-29 03:50:31 -0700

    Update Marketplace.h

--- include/Marketplace.h
+++ include/Marketplace.h
@@ -1,14 +1,10 @@
 #pragma once
-#include "OpenSeaClient.h"
+#include "json.hpp"
+#include <string>
 
-inline json getCollectionListings(OpenSeaClient& client,
-                                  const std::string& collectionSlug,
-                                  int limit = 50,
-                                  const std::string& next = "") {
-    std::string url = "https://api.opensea.io/api/v2/listings/collection/" +
-                      collectionSlug + "?limit=" + std::to_string(limit);
-    if (!next.empty()) {
-        url += "&next=" + next;
-    }
-    return client.get(url);
-}
+using json = Json;
+
+class Marketplace {
+public:
+    static void printListingInfo(const json& asset);
+};
