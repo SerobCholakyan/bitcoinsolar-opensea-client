73b17023229d02437a42423637222de93f14449d
SerobCholakyan <260147726+SerobCholakyan@users.noreply.github.com>
2026-03-29 04:19:51 -0700

    Update Metadata.cpp

--- src/Metadata.cpp
+++ src/Metadata.cpp
@@ -2,18 +2,21 @@
 
 std::string Metadata::getName(const json& asset) {
     auto it = asset.object().find("name");
-    if (it == asset.object().end() || !it->second.is_string()) return "";
-    return it->second.string();
+    return (it != asset.object().end() && it->second.is_string())
+           ? it->second.string()
+           : "";
 }
 
 std::string Metadata::getDescription(const json& asset) {
     auto it = asset.object().find("description");
-    if (it == asset.object().end() || !it->second.is_string()) return "";
-    return it->second.string();
+    return (it != asset.object().end() && it->second.is_string())
+           ? it->second.string()
+           : "";
 }
 
 std::string Metadata::getImageUrl(const json& asset) {
     auto it = asset.object().find("image_url");
-    if (it == asset.object().end() || !it->second.is_string()) return "";
-    return it->second.string();
+    return (it != asset.object().end() && it->second.is_string())
+           ? it->second.string()
+           : "";
 }
