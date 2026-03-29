4d3f9a8b781a4975de8488a9dff6daf3a0e6fc5b
SerobCholakyan <260147726+SerobCholakyan@users.noreply.github.com>
2026-03-29 03:49:16 -0700

    Refactor Gallery.h to remove getCollectionNFTs function
    Removed inline function for fetching NFTs and added Gallery class.

--- include/Gallery.h
+++ include/Gallery.h
@@ -1,14 +1,11 @@
 #pragma once
-#include "OpenSeaClient.h"
+#include <string>
+#include <vector>
+#include "json.hpp"
 
-inline json getCollectionNFTs(OpenSeaClient& client,
-                              const std::string& collectionSlug,
-                              int limit = 50,
-                              const std::string& next = "") {
-    std::string url = "https://api.opensea.io/api/v2/collection/" + collectionSlug +
-                      "/nfts?limit=" + std::to_string(limit);
-    if (!next.empty()) {
-        url += "&next=" + next;
-    }
-    return client.get(url);
-}
+using json = Json;
+
+class Gallery {
+public:
+    static void printAssets(const json& assets);
+};
